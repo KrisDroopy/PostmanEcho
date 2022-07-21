8a5f6972d3e5fbccf66b73b6015043d91f78b212
Кристина Косенкова <kr49385@gmail.com>
2022-07-21 17:38:30 +0300

    Initial commit

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class PostmanEchoTest {
+
+@Test
+    public void test() {
+    given()
+            .baseUri("https://postman-echo.com")
+            .body("KrisDroopy")
+
+            .when()
+            .post("/post")
+
+            .then()
+            .statusCode(200)
+//            .body("data", equalTo("Kris"))
+    ;
+}
+}
+
